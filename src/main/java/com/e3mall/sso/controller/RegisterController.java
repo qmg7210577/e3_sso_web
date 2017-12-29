4033b7e76b0431e9b30d418786a0eb6c7a806f66
qimenggao <qi7210577@qq.com>
2017-12-29 17:49:17 +0800

    Initial commit

---
+++ src/main/java/com/e3mall/sso/controller/RegisterController.java
@@ -0,0 +1,10 @@
+package com.e3mall.sso.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by qimenggao on 2017/12/29.
+ */
+@Controller
+public class RegisterController {
+}
